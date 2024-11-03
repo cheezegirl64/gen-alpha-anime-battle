959b65d475849739a646a865118985b990820c80
cheezegirl64 <hawterthanyou7@gmail.com>
2024-11-03 02:19:15 +0000

    deleted:    LICENSE 	new file:   index.html 	new file:   index.js 	new file:   show.js 	new file:   style.css

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log(`Hello`);
+console.log(`i farted`);
+function button(){
+    alert(`go away`);
+    alert(`i hate you`)
+    alert(`give me my donuts back`)
+    const whats_ligma = prompt(`i'm dangerous i have ligma grr (hint: either question the definition of one of the words, or invalidate mr. sigma's feelings :(...)`)
+    if (whats_ligma.toLowerCase() == `what's ligma?`) {
+        alert(`ligma sigma ba- *explodez*`)
+    } else if (whats_ligma.toLowerCase() == `womp womp`) {
+        alert(`*unlocks inner alpha* "yOU dARe attempT to uNSKibIdi me, little alpha... you... shall... BE STRIPPED OF YOUR GYATTSTER ROLE`)
+    } else {
+        alert(`you failed to answer correctly, you're now a dull little alpha`)
+    }
+    
+    const age = prompt(``)
+}
+
---
+++ show.js
@@ -0,0 +1,11 @@
+
+/**
+ * 
+ * Functions
+ * 
+ */
+function test(name, age) {
+    console.log(`Hello, my name is ${name} and I am ${age} years old.`)
+}
+
+test("Darcy", 14)
